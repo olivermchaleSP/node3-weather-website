e8aa59f5f61b67720d891906e2e1fd84075da683
OMchale <o.mchale@servicepower.com>
2022-03-24 15:15:26 +0000

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log('client side js loaded');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const message1 = document.querySelector('#message-1');
+const message2 = document.querySelector('#message-2');
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value;
+  console.log(location);
+  const url =
+    'http://localhost:3000/weather?address=' + encodeURIComponent(location);
+  message1.textContent = 'Loading...';
+  message2.textContent = '';
+  fetch(url).then((res) => {
+    res.json().then((data) => {
+      if (data.error) {
+        message1.textContent = data.error;
+      }
+      message1.textContent = data.location;
+      message2.textContent = data.forecast;
+    });
+  });
+});
